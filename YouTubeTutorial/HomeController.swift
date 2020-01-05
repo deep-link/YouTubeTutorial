ae04de8bee907dd9c5a75df0a4fd70ecdfe1d0a5
Russel Rajitha <russelrajitha@gmail.com>
2020-01-05 13:26:47 +0530

    initial for main layout containers

---
+++ YouTubeTutorial/HomeController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  YouTubeTutorial
+//
+//  Created by Admin on 5/1/20.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+import UIKit
+class HomeController: UICollectionViewController , UICollectionViewDelegateFlowLayout{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Home"
+        collectionView.backgroundColor = UIColor.white
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellId")
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
+        cell.backgroundColor=UIColor.red
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 200)
+    }
+    
+}
